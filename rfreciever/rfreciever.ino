66d6ec2566e1949bb85066fedcc5d383cf234077
rakshithhv <rakshith.hv741@gmail.com>
2017-09-08 20:01:25 +0530

    Arduino hardware code

---
+++ rfreciever/rfreciever.ino
@@ -0,0 +1,30 @@
+#include <VirtualWire.h>
+int buzzer = 9;      
+
+void setup()
+{
+  pinMode(buzzer,OUTPUT);
+  vw_set_ptt_inverted(true); //either transmitting or receiving 
+  vw_set_rx_pin(10); // set receiver pin
+  vw_setup(4000);  // Bits per sec
+  vw_rx_start();       // Start listening to the receiver
+}
+    void loop()
+{
+  digitalWrite(buzzer,LOW);
+  uint8_t buf[VW_MAX_MESSAGE_LEN]; // message length
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+  if (vw_get_message(buf, &buflen)) { // if message received
+    if(buf[0]=='X'){  // if the first letter in message array is X 
+       
+      for(int i=0;i<15;i++){        // loop to blink LED (Temporary setup instead of buzzer)
+        digitalWrite(buzzer,LOW);
+        delay(300);
+        digitalWrite(buzzer,HIGH);
+        delay(300);
+        }
+     }  
+    }
+}
+ 
---
+++ rftransmitter/rftransmitter.ino
@@ -0,0 +1,19 @@
+#include <VirtualWire.h>
+const char *message = "X" ;
+int PushButton = 9; // PushButton to activate RF PushButton
+
+void setup() {
+pinMode(PushButton,INPUT);
+
+vw_set_ptt_inverted(true); // either transmitting or receiving
+vw_set_tx_pin(10); // set transmitter pin
+vw_setup(4000);// speed of data transfer Kbps
+}
+
+void loop(){
+  
+if (digitalRead(PushButton) == HIGH)
+{  
+vw_send((uint8_t *)message, strlen(message)); // send the message
+}
+}
